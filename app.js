cec99eecb10c3e14c9665583d057fc7fa8478aa9
Rohan Shah <rohanshah945@gmail.com>
2021-05-08 16:44:29 +0530

    deploying to heroku

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 dotenv.config(); // To use environment variables
 
 // Middleware
-app.use(express.json({ limit: "10mb" })); // This is used for body-parser
+app.use(express.json({ limit: "15mb" })); // This is used for body-parser
 app.use(cors()); // This is used to enable CORS
 
 // mongoDB Config
